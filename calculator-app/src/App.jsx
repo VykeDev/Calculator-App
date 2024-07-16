2db2a70c35108064e193b1f1a2e52ff2ff0170e9
Rift <hegide.dev@gmail.com>
2024-07-15 22:32:33 -0400

    html

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="calculator-app">
+      <div className="screen">
+        <p className="sub-screen"></p>
+        <p className="main-screen"></p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="bord">
+        <button className="button">AC</button>
+        <button className="button">DE</button>
+        <button className="button">.</button>
+        <button className="button">/</button>
+        <button className="button">7</button>
+        <button className="button">8</button>
+        <button className="button">9</button>
+        <button className="button">*</button>
+        <button className="button">4</button>
+        <button className="button">5</button>
+        <button className="button">6</button>
+        <button className="button">-</button>
+        <button className="button">1</button>
+        <button className="button">2</button>
+        <button className="button">3</button>
+        <button className="button">+</button>
+        <button className="button">0</button>
+        <button className="button">=</button>
+        <button className="button">{"("}</button>
+        <button className="button">{")"}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
