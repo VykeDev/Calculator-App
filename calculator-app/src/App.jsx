e1d1aaece877c71ee8346204db464e57b6d051cf
Rift <hegide.dev@gmail.com>
2024-07-15 23:57:02 -0400

    add and remove

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -1,8 +1,35 @@
 import { useState } from "react";
 
+function AddButton(props) {
+  return (
+    <button
+      className="button"
+      onClick={(e) => props.addFunction(e)}
+      value={props.value}
+    >
+      {props.value}
+    </button>
+  );
+}
+
 function App() {
   const [subScreen, setSubScreen] = useState("sub-screen");
-  const [mainScreen, setMainScreen] = useState("main-screen");
+  const [mainScreen, setMainScreen] = useState("");
+
+  function add(e) {
+    setMainScreen((m) => m + e.target.value);
+  }
+
+  function remove() {
+    setMainScreen((m) => m.slice(0, -1));
+  }
+
+  function clear() {
+    setMainScreen("");
+    setSubScreen("");
+  }
+
+  function calculate() {}
   return (
     <div className="calculator-app">
       <div className="screen">
@@ -10,26 +37,32 @@ function App() {
         <p className="main-screen">{mainScreen}</p>
       </div>
       <div className="bord">
-        <button className="button">AC</button>
-        <button className="button">DE</button>
-        <button className="button">.</button>
-        <button className="button">/</button>
-        <button className="button">7</button>
-        <button className="button">8</button>
-        <button className="button">9</button>
-        <button className="button">*</button>
-        <button className="button">4</button>
-        <button className="button">5</button>
-        <button className="button">6</button>
-        <button className="button">-</button>
-        <button className="button">1</button>
-        <button className="button">2</button>
-        <button className="button">3</button>
-        <button className="button">+</button>
-        <button className="button">0</button>
-        <button className="button">=</button>
-        <button className="button">{"("}</button>
-        <button className="button">{")"}</button>
+        <button className="button" onClick={clear}>
+          AC
+        </button>
+        <button className="button" onClick={remove}>
+          DE
+        </button>
+        <AddButton value={"."} addFunction={add} />
+        <AddButton value={"/"} addFunction={add} />
+        <AddButton value={7} addFunction={add} />
+        <AddButton value={8} addFunction={add} />
+        <AddButton value={9} addFunction={add} />
+        <AddButton value={"*"} addFunction={add} />
+        <AddButton value={4} addFunction={add} />
+        <AddButton value={5} addFunction={add} />
+        <AddButton value={6} addFunction={add} />
+        <AddButton value={"-"} addFunction={add} />
+        <AddButton value={1} addFunction={add} />
+        <AddButton value={2} addFunction={add} />
+        <AddButton value={3} addFunction={add} />
+        <AddButton value={"+"} addFunction={add} />
+        <AddButton value={0} addFunction={add} />
+        <button className="button" onClick={calculate}>
+          =
+        </button>
+        <AddButton value={"("} addFunction={add} />
+        <AddButton value={")"} addFunction={add} />
       </div>
     </div>
   );
